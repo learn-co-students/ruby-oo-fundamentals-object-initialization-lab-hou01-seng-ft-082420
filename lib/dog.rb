f460257ca135c44780f8a0b8ca7512e88edb3d0e
Jaden Locke <jadenlocke1113@gmail.com>
2020-08-25 22:19:28 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog 
+  
+  def initialize(dogs_name, breed)
+    
+    @name = dogs_name
+    
+  def initialize(breed, dogs_name)
+    
+    @breed = dogs_name
+    
+    def initialize(breed)
+      
+      @breed = "Mutt"
+  
+end 
+end 
+end 
+end 
